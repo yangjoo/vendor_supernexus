eb4311e8712fd61d3f777b85df7cc12dd856cb4e
Faryaab Sheikh <faryaabsheikh@gmail.com>
2013-12-25 03:18:35 +0500

    Set default sounds

--- configs/common_phone.mk
+++ configs/common_phone.mk
@@ -9,3 +9,9 @@ PRODUCT_COPY_FILES += \
 # SIM Toolkit
 PRODUCT_PACKAGES += \
     Stk
+
+# Default sounds
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.config.ringtone=Orion.ogg \
+    ro.config.notification_sound=Argon.ogg \
+    ro.config.alarm_alert=Hassium.ogg
