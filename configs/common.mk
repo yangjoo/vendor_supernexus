a36fd5765a27973f4445eea54001df0748981164
joohyoung Yang <yangjoo@debian.yangjoo.net>
2014-11-05 22:36:05 +0900

    modify default date format

--- configs/common.mk
+++ configs/common.mk
@@ -10,7 +10,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.google.clientidbase=android-google \
     ro.com.android.wifi-watchlist=GoogleGuest \
     ro.setupwizard.enterprise_mode=1 \
-    ro.com.android.dateformat=MM-dd-yyyy \
+    ro.com.android.dateformat=yyyy-MM-dd \
     ro.com.android.dataroaming=false
 
 # Include SuperNexus boot animation
