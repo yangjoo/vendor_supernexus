1f8e2e4af405711eda608bc6b7d8e1ed719377cd
joohyoung Yang <yangjoo@debian.yangjoo.net>
2014-11-05 22:29:16 +0900

    remove unnecessary products config

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,15 +1,2 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/galaxysmtd.mk \
-    $(LOCAL_DIR)/i777.mk \
-    $(LOCAL_DIR)/i9100.mk \
-    $(LOCAL_DIR)/i9100g.mk \
-    $(LOCAL_DIR)/i9300.mk \
-    $(LOCAL_DIR)/i9305.mk \
-    $(LOCAL_DIR)/jflteatt.mk \
-    $(LOCAL_DIR)/jfltespr.mk \
-    $(LOCAL_DIR)/jfltetmo.mk \
-    $(LOCAL_DIR)/jflteusc.mk \
-    $(LOCAL_DIR)/jfltevzw.mk \
-    $(LOCAL_DIR)/jfltexx.mk \
-    $(LOCAL_DIR)/n7000.mk \
-    $(LOCAL_DIR)/n7100.mk
+    $(LOCAL_DIR)/i9300.mk
